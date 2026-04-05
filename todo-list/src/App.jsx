888a2cd03f8ab6c1897479570a1e078282793cab
ubaid2917 <ubaid29170@gmail.com>
2026-04-05 17:10:31 +0500

    completed todo list

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -9,15 +9,26 @@ function App() {
   function addTask(){
     if(task.trim() === "") return   
     
-    console.log("task", task)
-
-    setTasks([...tasks, task])
+    setTasks([...tasks, { text: task, completed: false }])
     setTask("")    
   } 
 
   function handleChange(e){ 
 
     setTask(e.target.value)
+  }
+
+  function toggleTask(index){
+    const updatedTasks = tasks.map((item, i) =>
+      i === index ? { ...item, completed: !item.completed } : item
+    )
+
+    setTasks(updatedTasks)
+  }
+
+  function deleteTask(index){
+    const updatedTasks = tasks.filter((_, i) => i !== index)
+    setTasks(updatedTasks)
   }
 
   return (
@@ -27,8 +38,8 @@ function App() {
         <h2 className='todo-container-h1'>Todo List</h2>  
 
         <div className='input-section'> 
-           <input type="text" value={task} onChange={handleChange} placeholder='Enter your task here' /> 
-           <button onClick={addTask}>Add</button>
+           <input type="text" className='input' value={task} onChange={handleChange} placeholder='Enter your task here' /> 
+           <button className='add-btn' onClick={addTask}>+</button>
         </div>  
 
         <div className='list-section'>
@@ -36,7 +47,37 @@ function App() {
 
           <ul>
             {tasks.map((t,index) => {
-             return <li className='last-list' key={index}>{t}</li>
+             return (
+              <li className='last-list' key={index}>
+                <input
+                  type="checkbox"
+                  checked={t.completed}
+                  onChange={() => toggleTask(index)}
+                />
+                <span className={t.completed ? 'task-text completed' : 'task-text'}>
+                  {t.text}
+                </span>
+                <button className='delete-btn' onClick={() => deleteTask(index)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M3 6h18" />
+                    <path d="M8 6V4h8v2" />
+                    <path d="M19 6l-1 14H6L5 6" />
+                    <path d="M10 11v6" />
+                    <path d="M14 11v6" />
+                  </svg>
+                </button>
+              </li>
+             )
             })}
           </ul>
         </div>
