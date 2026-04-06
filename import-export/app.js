b1c90ea4fee7be1e112e18e9f9a1ebad88e7abe1
ubaid2917 <ubaid29170@gmail.com>
2026-04-06 22:41:17 +0500

    update export and imporyt

---
+++ import-export/app.js
@@ -0,0 +1,15 @@
+const user = {
+    name: 'John Doe',
+    age: 30,
+    email: 'john@example.com'
+}   
+
+
+export const a = 10; 
+
+ 
+
+const arr = [10,20,30,40,50]; 
+
+
+export default user;
---
+++ import-export/script.js
@@ -0,0 +1,7 @@
+import { a } from './app.js'
+import user from './app.js'
+
+console.log('hello')
+console.log(a)
+console.log('user:', user)
+
