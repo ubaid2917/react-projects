c9652e678009db6125f770bab0236736a61a61cc
ubaid2917 <ubaid29170@gmail.com>
2026-04-06 23:35:36 +0500

    create-first-app and fraggment and folder structure

---
+++ first-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h2>Hello from first div</h2>
+      </div>
+      <div>
+          <h2>Hello from second div</h2>
+      </div>
+    </>
+
+  )
+}
+
+export default App
