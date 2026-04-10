7f129a55ce4fd322ca34e5d02048953ed6747227
ubaid2917 <ubaid29170@gmail.com>
2026-04-10 06:33:43 +0500

    working on counter app css

--- react-template/src/App.jsx
+++ react-template/src/App.jsx
@@ -1,40 +1,54 @@
 import React, { useState } from "react";
 import Counter from "./counter";
-
+import "./App.css"
 
 function App() {
 
   // variables
-  const age = 20;   
+  // const age = 20;   
 
-  // use state
-   const [name, setName] = useState('Ubaid');
+  // // use state
+  //  const [name, setName] = useState('Ubaid');
 
 
-  // function 
-  const changeUser = ()=>{
-    setName("Ubaid Naeem")
-  } 
+  // // function 
+  // const changeUser = ()=>{
+  //   setName("Ubaid Naeem")
+  // } 
 
-  // default User name  
-  const setDefault = () => {
-    setName("Ubaid")
-  }
+  // // default User name  
+  // const setDefault = () => {
+  //   setName("Ubaid")
+  // }
 
+
+  // return (
+  //   <>
+  //     {/* variable calling */}
+  //     <h1>Hi {name}, your age is {age}</h1>
+
+  //     <button onClick={changeUser}>Change Name</button> 
+
+  //     <button onClick={setDefault}>Set Default</button> 
+
+
+
+  //       <Counter/>
+
+
+  //   </>
+  // )  
+
+
+  //*  =========== Counter App ===========    
+  const [counter, setCounter] = useState(0);
 
   return (
     <>
-      {/* variable calling */}
-      <h1>Hi {name}, your age is {age}</h1>
+      <div className="container">
+        <h3>Counter App</h3>
+      </div>
 
-      <button onClick={changeUser}>Change Name</button> 
-
-      <button onClick={setDefault}>Set Default</button> 
-
-
-        
-        <Counter/>
-      
 
     </>
   )
