246318e2892815c17038f04cf1b06bd29811614e
ubaid3720 <ubaid@syncscrum.com>
2026-04-10 17:34:31 +0500

    tailwind form design start

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -1,8 +1,21 @@
-function App(){
+function App() {
+
+   function handleSubmit(e) {
+      console.log('heelooo')
+      e.preventDefault()
+   }
    return (
-    <>
-    <h1 className="text-xl bg-red-400">App Component</h1>
-    </>
+      <>
+         <div className="bg-black-300  h-screen flex items-center justify-center ">
+            <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center  gap-10 max-w-[500px] mx-auto w-full rounded-xl shadow-xl/20  bg-mist-400 h-1/2">
+               <input type="text" className="px-4 py-3" placeholder="Enter your name" />
+
+
+               <input type="text" className="px-4 py-3" placeholder="Enter your email" />
+               <button className="px-4 py-2 text-xl font-semibold bg-blue-500 text-white border border-black-300 rounded ">submit</button>
+            </form>
+         </div>
+      </>
    )
 }
 export default App;
