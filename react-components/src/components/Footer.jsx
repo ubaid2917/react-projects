b2e7330ab6ed6fd3b07e279997f4abab91b4fbb8
ubaid2917 <ubaid29170@gmail.com>
2026-04-15 10:44:42 -0700

    footer components

---
+++ react-components/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='bg-emerald-950 text-white px-5 py-5 flex items-center justify-center mt-5 absolute bottom-0 w-full'>
+            <p>&copy; 2023 Ubaid Naeem. All rights reserved.</p>
+        </footer>
+
+    </div>
+  )
+}   
+
+export default Footer
