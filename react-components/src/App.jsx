7c0c8dd7bc5026206f6617d658e9ccdf2e055f46
ubaid2917 <ubaid29170@gmail.com>
2026-04-15 10:49:06 -0700

    footer components

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Main from './components/Main'
 
 const App = () => {
   return (
    <>  
     <Header />
+    <Main />
     <Footer />
    </>
   )
---
+++ react-components/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold text-center mt-5'>Welcome to My Website</h1>
+        <p className='text-center mt-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  )
+}
+
+export default Main
