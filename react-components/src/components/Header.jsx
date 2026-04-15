1145c7a2c956c88eb10a0ab03f1ace681530b7a2
ubaid2917 <ubaid29170@gmail.com>
2026-04-15 10:42:36 -0700

    updated

---
+++ react-components/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+     <> 
+   
+     <nav className='bg-emerald-950 text-white px-5 py-5 flex items-center justify-between'>
+        <h1>Ubaid Naeem</h1>  
+
+        <div>
+          <ul className='flex gap-9 mt-3 items-center'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Product</li>
+            <li>Services</li>
+          </ul>
+        </div>
+     </nav>
+     </>
+  )
+}
+
+export default Header
