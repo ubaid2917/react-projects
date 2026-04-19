2400d50669987a6fc6a27ab152de3bcca22ca07d
ubaid2917 <ubaid29170@gmail.com>
2026-04-19 10:23:03 +0500

    update props code

--- projects/props-project/src/components/Product.jsx
+++ projects/props-project/src/components/Product.jsx
@@ -3,55 +3,85 @@ import React from 'react'
 export const Product = () => {
 
     let products = [
-  {
-    id: 1,
-    title: "Laptop",
-    price: 1000,
-    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
-    description: "High-performance laptop suitable for work, gaming, and everyday tasks."
-  },
-  {
-    id: 2,
-    title: "Phone",
-    price: 500,
-    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9",
-    description: "Modern smartphone with excellent camera and long battery life."
-  },
-  {
-    id: 3,
-    title: "Headphones",
-    price: 100,
-    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
-    description: "Comfortable headphones with high-quality sound and noise isolation."
-  },
-  {
-    id: 4,
-    title: "Watch",
-    price: 200,
-    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
-    description: "Stylish wristwatch with premium design and durable build."
-  }
-];
+        {
+            id: 1,
+            title: "Laptop",
+            price: 1000,
+            image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
+            description: "High-performance laptop suitable for work, gaming, and everyday tasks."
+        },
+        {
+            id: 2,
+            title: "Phone",
+            price: 500,
+            image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9",
+            description: "Modern smartphone with excellent camera and long battery life."
+        },
+        {
+            id: 3,
+            title: "Headphones",
+            price: 100,
+            image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
+            description: "Comfortable headphones with high-quality sound and noise isolation."
+        },
+        {
+            id: 4,
+            title: "Watch",
+            price: 200,
+            image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
+            description: "Stylish wristwatch with premium design and durable build."
+        },
+         {
+            id: 1,
+            title: "Laptop",
+            price: 1000,
+            image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
+            description: "High-performance laptop suitable for work, gaming, and everyday tasks."
+        },
+        {
+            id: 2,
+            title: "Phone",
+            price: 500,
+            image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9",
+            description: "Modern smartphone with excellent camera and long battery life."
+        },
+        {
+            id: 3,
+            title: "Headphones",
+            price: 100,
+            image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
+            description: "Comfortable headphones with high-quality sound and noise isolation."
+        },
+        {
+            id: 4,
+            title: "Watch",
+            price: 200,
+            image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
+            description: "Stylish wristwatch with premium design and durable build."
+        }
+    ];
 
 
     return (
         <>
-            <div className='flex '>
+            <h1 className='text-2xl m-4 text-gray-800 text-center'>Products List</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6'>
+
                 {
                     products.map((product) => {
                         return (
                             <div className='bg-gray-100 shadow-md rounded-lg p-4 border-gray-500 mt-2 mx-3 border-2 w-96'>
-                                <img className=' object-contain' src={product.image} alt={product.title} />
-                                <h1>{product.title}</h1>
+                                <img className=' object-cover h-75 w-full rounded' src={product.image} alt={product.title} />
+                                <h1 className='mt-2'>{product.title}</h1>
                                 <p className='text-gray-600'>{product.description}</p>
-                                 
-                                 <div className='flex items-center justify-between mt-5'>
+
+                                <div className='flex items-center justify-between mt-5'>
                                     <h3>$ {product.price.toFixed(2)}</h3>
                                     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-                                    Add to Cart
-                                </button>
-                                 </div>
-                                
+                                        Add to Cart
+                                    </button>
+                                </div>
+
                             </div>
                         )
 
