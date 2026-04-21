8464fa84afad4d103e4a40daa01bbc9b57c3ebfe
ubaid29170 <ubaid.naeem@masjidal.com>
2026-04-21 08:22:53 +0500

    work on react router dom

--- projects/react-router-dom/src/routes/AppRoutes.jsx
+++ projects/react-router-dom/src/routes/AppRoutes.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import About from '../pages/About'
 import Product from '../pages/Product'
 import Gallery from '../pages/Gallery'
-
+import Contact from '../pages/Contact'
+import Home from '../pages/Home'
 const AppRoutes = () => {
     return (
         <Routes>
@@ -11,7 +12,11 @@ const AppRoutes = () => {
 
             <Route path="/products" element={<Product />} />  
 
-            <Route path='/gallery' element={<Gallery/>} />
+            <Route path='/gallery' element={<Gallery/>} />  
+
+            <Route path='/contact' element={<Contact/>} />   
+
+            <Route path='/' element={<Home/>} />
 
         </Routes>
     )
