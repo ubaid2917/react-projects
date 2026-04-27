8b8a52cd37f9eaeb6e401a3f19ab9e88b219ca0b
ubaid3720 <ubaid@syncscrum.com>
2026-04-27 17:43:24 +0500

    working on product carts

---
+++ redux-toolkit/redux projects/product-carts/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavbarComponent from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <>
+         <NavbarComponent/>
+        <h1>Product Carts</h1> 
+     
+      </>
+    </div>
+  )
+}
+
+export default App
---
+++ redux-toolkit/redux projects/product-carts/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const NavbarComponent = () => {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="px-3 d-flex justify-content-between">
+        <Navbar.Brand href="#home" className="fw-bold">Product Cart</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className='d-flex justify-content-end w-100'>
+            <Nav.Link href="#home" className="text-light">Home</Nav.Link>
+            <Nav.Link href="#features" className="text-light">Features</Nav.Link>
+            <Nav.Link href="#pricing" className="text-light">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+     
+
+    </>
+  )
+}
+
+export default NavbarComponent
